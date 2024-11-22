851ffadb1232d3a63280e34e415d335640003bf7
Selva <kselvakumar@dmdbrands.com>
2024-11-22 10:10:02 +0530

    bug fixes

--- util/initializeRoutes.js
+++ util/initializeRoutes.js
@@ -3,9 +3,7 @@ import { fileURLToPath, pathToFileURL } from "url";
 import request_auth from "../middleware/request_auth.js";
 import swaggerUi from "swagger-ui-express";
 import path from "path";
-import fs from "fs";
-import swaggerValidation from "openapi-validator-middleware";
-import api from "suppress-experimental-warnings";
+import open_api_validator from "openapi-validator-middleware";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -31,23 +29,28 @@ async function initializeSwagger(swaggerPath, baseRoute, server) {
     name: currentSwaggerDoc.info.title,
   });
 
-  swaggerValidation.init(currentSwaggerDoc);
-  const userValidation = swaggerValidation.getNewMiddleware(currentSwaggerDoc);
-  return userValidation;
+  open_api_validator.init(currentSwaggerDoc);
+  const swaggerValidation =
+    open_api_validator.getNewMiddleware(currentSwaggerDoc);
+  return swaggerValidation;
 }
 
-async function initializeRoutes(userValidation, baseRoute, filePath, server) {
+async function initializeRoutes(
+  routePath,
+  baseRoute,
+  server,
+  swaggerValidation
+) {
   console.log(`\x1b[32mAssigning routes for: /${baseRoute}\x1b[0m`);
-  const { default: apiRoutes } = await import(pathToFileURL(filePath));
+  const { default: apiRoutes } = await import(pathToFileURL(routePath));
 
-  // Apply middleware for all non-authentication endpoints
   apiRoutes.stack.forEach((route) => {
     if (route.route) {
       const apiPath = route.route.path;
       if (!["/login", "/register", "/refresh", "/addAdmin"].includes(apiPath)) {
         server.use(`/${baseRoute}${apiPath}`, request_auth);
       }
-      server.all(`/${baseRoute}${apiPath}`, userValidation.validate);
+      server.all(`/${baseRoute}${apiPath}`, swaggerValidation.validate);
     }
   });
   server.use(`/${baseRoute}`, apiRoutes);
@@ -59,23 +62,27 @@ async function initializeMiddleware(server) {
   try {
     const routes = await recursive("./routes", ["*.swagger.json"]);
 
-    for (const filePath of routes) {
-      const parentDirectory = path.dirname(filePath);
-      const swaggerPath = await recursive(parentDirectory, ["*.js"]);
-      const fileName = path.basename(filePath);
+    for (const routePath of routes) {
+      const fileName = path.basename(routePath);
       const baseRoute = fileName.split(".").slice(0, 1);
-      const userValidation = await initializeSwagger(
-        swaggerPath[0],
+
+      //Initializing Swagger
+      const parentDirectory = path.dirname(routePath);
+      const swaggerPath = (await recursive(parentDirectory, ["*.js"]))[0];
+      const swaggerValidation = await initializeSwagger(
+        swaggerPath,
         baseRoute,
         server
       );
-      await initializeRoutes(userValidation, baseRoute, filePath, server);
-      // Setup Swagger UI
-      server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(null, options));
-      console.log(
-        "\x1b[32mSwagger documentation available at: http://192.168.1.13:3000/api-docs\x1b[m"
-      );
+
+      //Initializing Routes
+      await initializeRoutes(routePath, baseRoute, server, swaggerValidation);
     }
+    // Setup Swagger UI
+    server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(null, options));
+    console.log(
+      "\x1b[32mSwagger documentation available at: http://192.168.1.13:3000/api-docs\x1b[m"
+    );
   } catch (err) {
     console.error("\x1b[31mError initializing routes:\x1b[0m", err);
   }
